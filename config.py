53681c4058b7db166b0dc7b60dfe34b09b572cd3
Robert0030 <119852862+Robert0030@users.noreply.github.com>
2022-12-18 17:38:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1759988677:AAHvico4csO7k89kZBpCLQC1wXiZWytR-Qc")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5967125243:AAGoIjWQO8_eo_uqkBGGwsTFByVMlaxFlHk")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 1824666))
-    API_HASH = os.environ.get("API_HASH" "d316eb966e164eb8783583025a27e530")
+    APP_ID = int(os.environ.get("APP_ID", 27884084))
+    API_HASH = os.environ.get("API_HASH" "f41ef10f7e283ba0b6b18fac6fbe8226")
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "cartoon_seriesz")
     # log channel
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "pokegozone")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "vedigundi_LinkZz")
     # Get these values from my.telegram.org
-    CHAT_ID = os.environ.get("CHAT_ID", "")
+    CHAT_ID = os.environ.get("CHAT_ID", "1899869233")
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "677682427").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5771348361").split())
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
@@ -38,5 +38,5 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "postgres://wmynzqfvxqvhah:78b6b1191cd6d4ae196239a2e091d43cff30e5fe6bbbec7b48e683ff8b57ec3e@ec2-54-85-80-92.compute-1.amazonaws.com:5432/dbuuofqgegto4u")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://olojoqlu:Sr7Fq5ow9tkV6p-dM4gW4ix4aukLKxij@peanut.db.elephantsql.com/olojoqlu")
     
